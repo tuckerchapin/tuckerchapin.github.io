ea813e1eda9154d725789f235a231cc3ba106752
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-11-20 22:10:00 -0500

    Updated to center on page.

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ class App extends Component {
     render() {
         return (
             <div id="app-container">
-                <div id="logo-container">
-                    <img className="logo glow" src="/logos/logo_clear.png" alt="glow"/>
-                    <img className="logo" src="/logos/logo_clear.png" alt="logo"/>
+                <div id="header-container">
+                    <div id="title">Tucker Chapin</div>
+                    <div id="contacts">
+                        {content.contacts.map((contact, i) => <Contact key={i} {...contact}/>)}
+                    </div>
                 </div>
-                <div id="info-container">
-                    <div id="header-container">
-                        <div id="title">Tucker Chapin</div>
-                        <div id="contacts">
-                            {content.contacts.map((contact, i) => <Contact key={i} {...contact}/>)}
-                        </div>
+                <div id="content-container">
+                    <div id="logo-container">
+                        <img className="logo glow" src="/logos/logo_clear.png" alt="glow"/>
+                        <img className="logo" src="/logos/logo_clear.png" alt="logo"/>
                     </div>
                     <div id="projects">
                         {content.projects.map((project, i) => <Project key={i} {...project}/>)}
