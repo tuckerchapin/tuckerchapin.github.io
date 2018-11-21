3e2d3f906d7e87fe7220c5750b38c51168b258bb
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-11-20 20:12:57 -0500

    Neon title.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
                 <img className="logo" src="/logos/logo_clear.png" alt="logo"/>
             </div>
             <div id="info-container">
-                <div id="name" className="title">Tucker Chapin</div>
+                <div id="title-container">
+                    <div className="title glow">Tucker Chapin</div>
+                    <div className="title">Tucker Chapin</div>
+                </div>
             </div>
         </div>
     );
