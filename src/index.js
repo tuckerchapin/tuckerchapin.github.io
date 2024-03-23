2594253569e1a4acb138fe70df53939f6c1296db
Tucker Chapin <tuckerchapin@gmail.com>
2024-03-23 19:44:20 -0400

    ok?

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { marked } from "marked"
+const marked = require("marked").marked
 console.log('Hello, world!')
 console.log("marked", marked("I am using __markdown__."))
 console.log('hmm thats weird that it exited')
