002f1bccbefbf73c121fe4921e58e354d93c970b
Tucker Chapin <tuckerchapin@gmail.com>
2024-03-23 19:45:07 -0400

    mayube we need await

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const marked = require("marked").marked
 console.log('Hello, world!')
-console.log("marked", marked("I am using __markdown__."))
+console.log("marked", await marked("I am using __markdown__."))
 console.log('hmm thats weird that it exited')
