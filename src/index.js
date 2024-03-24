e377314d7868f4d88afdef0c08e711132c0d277e
Tucker Chapin <tuckerchapin@gmail.com>
2024-03-23 20:08:07 -0400

    does this produce files?

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import { marked } from 'marked'
 import fs from 'fs'
 
-const issues = fs.readdirSync('issues')
-for (issue of issues) {
-  const content = fs.readFileSync(`issues/${issue}`, 'utf8')
-  const html = marked(content)
-  console.log('issue', html)
+// TODO handle startup, no issues folder
+
+if (!fs.existsSync('issues')) {
+  console.log('No issues/ folder found. Exiting.');
+  process.exit();
+}
+
+const issueFiles = fs.readdirSync('issues')
+for (const issueFile of issueFiles) {
+  const content = JSON.parse(fs.readFileSync(`issues/${issueFile}`, 'utf8'))
+  if (content.body) {
+    const html = `<html><body>${marked(content.body)}</body></html>`;
+    if (!fs.existsSync('blog')) fs.mkdirSync('blog');
+    fs.writeFileSync(`blog/${issueFile}.html`, html);
+    console.log(issueFile, `blog/${issueFile}.html`)
+  } else {
+    console.log(issueFile, 'no body')
+  }
 }
